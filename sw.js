a60830e972bf0b2955063c2bbe83a0942fcdf00c
Carincrack <axelramos082@gmail.com>
2025-06-28 03:16:02 -0600

    Nueva Vercion

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@ self.addEventListener('install', (event) => {
   );
   self.skipWaiting();
 });
-
+ 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request).catch(() => caches.match('/index.html')))
